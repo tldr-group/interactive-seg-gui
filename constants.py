339a6a56bc5bc7bf981edb1eb0d096eb9d2e44e6
rmdocherty <rmdocherty2@gmai.com>
2025-03-03 22:20:30 +0000

    constants

---
+++ constants.py
@@ -0,0 +1,28 @@
+from typing import Literal
+
+COLOURS: list[str] = [
+    "#fafafa",
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+]
+CANVAS_W: int = 850
+CANVAS_H: int = 1200
+CANVAS_H_GRID: int = 5
+CANVAS_W_GRID: int = 1
+
+PAD: int = 10
+MENU_BAR_ROW: int = 0
+SIDE_COL: int = 2
+
+MIN_TIME: float = 0.07
+
+MessageTypes = Literal["NOTIF", "TRAIN", "SEGMENT"]
+ProgressTypes = Literal["start", "stop", "progress", "N/A"]
